c2331d4860a29aec4158f66fd089e545fcbe25cb
DI <DI@DESKTOP-FO43F3V>
2018-11-06 17:47:14 -0300

    Adicionei a tela do administrador ,tabela , botoes etc..

---
+++ web/telaFuncionariosADM.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Starter Template - Materialize</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav class="light-blue lighten-1" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo"></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#"></a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+  <div class="section  " id="">
+    <div class="container row ">
+
+        <form class="col s12">
+          <div class="row">
+            <div class="input-field col s6 offset-s3 ">
+              <i class="material-icons prefix blue-text">search</i>
+              <input id="icon_prefix" type="text" class="validate ">
+              <label class="" for="icon_prefix"></label>
+            </div>
+            <div class="input-field col s3 ">
+              <a  class="btn-floating btn-small waves-effect waves-light blue"><i class="material-icons">add</i></a>
+
+            </div>
+          </div>
+        </form>
+
+    </div>
+  </div>
+  <div class="center">
+    <h3>Funcionarios</h3>
+
+  </div>
+
+  <div class="container">
+    <div class="section center">
+
+      <!--   Icon Section   -->
+      CPF, RG, seu
+      nome, seu endereço e seu cargo.
+      <table class="highlight center">
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Cargo</th>
+          <th></th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+          <td>Alvin</td>
+          <td>Eclair</td>
+          <td> <a  class="btn-floating btn-small waves-effect waves-light blue"><i class="material-icons">contacts</i></a><a  class="btn-floating btn-small waves-effect waves-light orange"><i class="material-icons">create</i></a></td>
+        </tr>
+        <tr>
+          <td>Alan</td>
+          <td>Jellybean</td>
+          <td> <a  class="btn-floating btn-small waves-effect waves-light blue"><i class="material-icons">contacts</i></a><a  class="btn-floating btn-small waves-effect waves-light orange"><i class="material-icons">create</i></a></td>
+        </tr>
+        <tr>
+          <td>Jonathan</td>
+          <td>Lollipop</td>
+          <td> <a  class="btn-floating btn-small waves-effect waves-light blue"><i class="material-icons">contacts</i></a><a  class="btn-floating btn-small waves-effect waves-light orange"><i class="material-icons">create</i></a></td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <br><br>
+  </div>
+
+  <footer class="page-footer blue">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
