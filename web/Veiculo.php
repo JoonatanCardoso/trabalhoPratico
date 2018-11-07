ddef8700933b442ff1ae9222be9302a67acfa47f
EduardoM <EduardoM@LAB49-03.lab.unigran>
2018-11-07 19:35:26 -0300

    Inserir a tela de cadastro veiculo

---
+++ web/Veiculo.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Starter Template - Materialize</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">LocalPlus</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">Cadastro de Veículo</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="placa" type="text" class="">
+          <label >Placa</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="marca" type="text" class="">
+          <label for="marca">Marca</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="modelo" type="text" class="">
+          <label for="modelo">Modelo</label>
+        </div>
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="cor" type="text" class="">
+          <label for="cor">Cor</label>
+        </div>
+		</div>
+      </div>
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="seguro" type="text" class="">
+          <label for="seguro">Valor do Seguro</label>
+			  
+        </div>
+		<div class="input-field col s6">
+          <input id="locacao" type="text" class="">
+          <label for="locacao">Valor da Locação</label>
+        </div>
+      </div>
+      
+    </form>
+  </div>
+  </body>
+</html>
